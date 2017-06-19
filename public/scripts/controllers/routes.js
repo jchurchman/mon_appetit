415c06f7d98867c167df71921637e9875db667b2
Joe Churchman <churchman.joe@gmail.com>
2017-06-19 16:51:34 -0700

    Fill out index.html, rough in splash page view

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -3,7 +3,7 @@
 var app = app || {};
 page('/', init);
 page('/dashboard', init);
-page('/yourRecipes', init);
+page('/myRecipes', init);
 page('/searchRecipes', init);
 
 page();
---
+++ public/scripts/views/appViewer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = app || {};
+
+(function (module) {
+    const appViewer = {};
+
+    appViewer.initIndexApp = () => {
+
+    }
+
+    module.appViewer = appViewer;
+}(app));
