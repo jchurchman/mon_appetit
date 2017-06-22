04e761707b8b43be0ab9f0115dfffee86592087a
Joe Churchman <churchman.joe@gmail.com>
2017-06-22 15:08:40 -0700

    updated database connection variable name for Heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 
-const conString = process.env.PG_CONSTRING;
+const conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
